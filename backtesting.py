4f3ba3e09a1845fdc8740520ad36fa555c624dab
nikso91640 <nicolaslegeay@hotmail.fr>
2024-01-04 17:53:49 +0100

    Update backtesting.py

--- backtesting.py
+++ backtesting.py
@@ -201,7 +201,6 @@ def main():
         years_difference = resultats[3]
         montant_final_max = resultats[4]
         interets = montant_final_max - resultats[0][meilleure_frequence]['total_investissement']
-        lumpSum = lumpSum['investissement']
 
         # Affichage des informations récapitulatives
         st.write(f"ETF : {ticker}")
